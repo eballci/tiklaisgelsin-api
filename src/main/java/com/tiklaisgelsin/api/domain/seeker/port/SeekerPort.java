6dce401d171e561232bbc3f160e60f5da40b3061
Emre BALCI <emre-balci@outlook.com.tr>
2023-04-28 23:16:50 +0300

    Update port and use case for getting a seeker

--- src/main/java/com/tiklaisgelsin/api/domain/seeker/port/SeekerPort.java
+++ src/main/java/com/tiklaisgelsin/api/domain/seeker/port/SeekerPort.java
@@ -1,10 +1,12 @@
 package com.tiklaisgelsin.api.domain.seeker.port;
 
 import com.tiklaisgelsin.api.domain.seeker.model.Seeker;
+import com.tiklaisgelsin.api.domain.seeker.usecase.seeker.GetSeeker;
 import com.tiklaisgelsin.api.domain.seeker.usecase.seeker.SeekerCreate;
 import com.tiklaisgelsin.api.domain.seeker.usecase.seeker.SeekerUpdate;
 
 public interface SeekerPort {
+    Seeker getSeeker(GetSeeker getSeeker);
     Seeker createSeeker(SeekerCreate seekerCreate);
 
     Void updateSeeker(SeekerUpdate seekerUpdate);
