cf5b38baf2e1f01efd7edff759389872fa04a9b2
Emre BALCI <emre-balci@outlook.com.tr>
2023-04-28 15:21:16 +0300

    Create spring boot project

---
+++ src/main/java/com/tiklaisgelsin/api/TiklagelsinApiApplication.java
@@ -0,0 +1,13 @@
+package com.tiklaisgelsin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiklagelsinApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiklagelsinApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tiklaisgelsin/api/TiklagelsinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiklaisgelsin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiklagelsinApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
