f13ec37f8037a4abb24f3717cdfeaaf5fb8481b9
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-07 17:09:40 +0300

    Fulfill employer model

--- src/main/java/com/tiklaisgelsin/api/domain/common/model/Employer.java
+++ src/main/java/com/tiklaisgelsin/api/domain/common/model/Employer.java
@@ -3,10 +3,18 @@ package com.tiklaisgelsin.api.domain.common.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Builder
 public class Employer {
     private Long id;
     private String avatar;
     private String name;
+    private String description;
+    private int scale;
+    private String webSite;
+    private List<Position> openPositions;
+    private List<Submission> submissions;
+    private List<SeekerSuggestion> seekerSuggestions;
 }
