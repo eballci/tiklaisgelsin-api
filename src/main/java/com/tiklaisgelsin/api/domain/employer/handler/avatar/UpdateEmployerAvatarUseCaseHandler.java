6908894d8ff484af5373199ce941beb77c16404c
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-08 13:32:20 +0300

    Create avatar updating use case handler

---
+++ src/main/java/com/tiklaisgelsin/api/domain/employer/handler/avatar/UpdateEmployerAvatarUseCaseHandler.java
@@ -0,0 +1,17 @@
+package com.tiklaisgelsin.api.domain.employer.handler.avatar;
+
+import com.tiklaisgelsin.api.domain.common.usecase.UseCaseHandler;
+import com.tiklaisgelsin.api.domain.employer.port.AvatarPort;
+import com.tiklaisgelsin.api.domain.employer.usecase.avatar.UpdateEmployerAvatar;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UpdateEmployerAvatarUseCaseHandler implements UseCaseHandler<Void, UpdateEmployerAvatar> {
+
+    private final AvatarPort avatarPort;
+
+    @Override
+    public Void handle(UpdateEmployerAvatar useCase) {
+        return avatarPort.updateSeekerAvatar(useCase);
+    }
+}
