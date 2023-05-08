8451cebfbefcf058bbea3e1195fe9fc0b48108d3
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-08 16:37:30 +0300

    Create position suggestion use case handler

---
+++ src/main/java/com/tiklaisgelsin/api/domain/common/handler/SuggestPositionUseCaseHandler.java
@@ -0,0 +1,44 @@
+package com.tiklaisgelsin.api.domain.common.handler;
+
+import com.tiklaisgelsin.api.domain.common.model.Criteria;
+import com.tiklaisgelsin.api.domain.common.model.Position;
+import com.tiklaisgelsin.api.domain.common.model.Seeker;
+import com.tiklaisgelsin.api.domain.common.port.SuggestionPort;
+import com.tiklaisgelsin.api.domain.common.usecase.SuggestPosition;
+import com.tiklaisgelsin.api.domain.common.usecase.UseCaseHandler;
+import com.tiklaisgelsin.api.domain.employer.port.PositionPort;
+import com.tiklaisgelsin.api.domain.seeker.port.SeekerPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+
+import java.util.List;
+import java.util.concurrent.Future;
+
+@RequiredArgsConstructor
+public class SuggestPositionUseCaseHandler implements UseCaseHandler<Future<?>, SuggestPosition> {
+
+    private final PositionPort positionPort;
+    private final SeekerPort seekerPort;
+    private final SuggestionPort suggestionPort;
+
+    @Async
+    @Override
+    public Future<?> handle(SuggestPosition useCase) {
+        Position position = positionPort.getPosition(useCase.getPositionId());
+        List<Seeker> seekers = seekerPort.getAllSeekers();
+
+        for (Seeker seeker : seekers) {
+            int sum = 0;
+
+            for (Criteria criteria : position.getCriteriaList()) {
+                sum += criteria.getPoint(seeker);
+            }
+
+            if (sum / position.getCriteriaList().size() >= 50) {
+                suggestionPort.createPositionSuggestion(position.getId(), seeker.getId());
+            }
+        }
+
+        return null;
+    }
+}
