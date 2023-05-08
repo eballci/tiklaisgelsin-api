03fddbfa72db74e1056586e7b5f66f211dbff0f8
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-08 17:01:20 +0300

    Create submission port for handling use case possible

---
+++ src/main/java/com/tiklaisgelsin/api/domain/seeker/port/SubmissionPort.java
@@ -0,0 +1,11 @@
+package com.tiklaisgelsin.api.domain.seeker.port;
+
+import com.tiklaisgelsin.api.domain.common.model.Submission;
+import com.tiklaisgelsin.api.domain.seeker.usecase.submission.IssueSubmission;
+import com.tiklaisgelsin.api.domain.seeker.usecase.submission.RemoveSubmission;
+
+public interface SubmissionPort {
+    Submission issueSubmission(IssueSubmission issueSubmission);
+
+    Void removeSubmission(RemoveSubmission removeSubmission);
+}
