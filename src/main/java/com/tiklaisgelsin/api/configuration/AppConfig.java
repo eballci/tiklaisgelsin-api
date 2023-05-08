8dc0f224e9bedc63c04229c8214a64fecd07d984
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-08 14:19:22 +0300

    Create app config for async tasks

---
+++ src/main/java/com/tiklaisgelsin/api/configuration/AppConfig.java
@@ -0,0 +1,24 @@
+package com.tiklaisgelsin.api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AppConfig {
+
+    @Bean
+    public TaskExecutor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("SuggestionThreads-");
+        executor.initialize();
+        return executor;
+    }
+
+}
