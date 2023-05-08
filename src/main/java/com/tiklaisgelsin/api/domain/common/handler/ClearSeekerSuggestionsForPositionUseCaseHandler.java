af608efeda27aace39344640ef061cadebbf92a9
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-08 16:43:22 +0300

    Create use case handler for cleaning seeker suggestions for updated positions

---
+++ src/main/java/com/tiklaisgelsin/api/domain/common/handler/ClearSeekerSuggestionsForPositionUseCaseHandler.java
@@ -0,0 +1,17 @@
+package com.tiklaisgelsin.api.domain.common.handler;
+
+import com.tiklaisgelsin.api.domain.common.port.SuggestionPort;
+import com.tiklaisgelsin.api.domain.common.usecase.ClearSeekerSuggestionsForPosition;
+import com.tiklaisgelsin.api.domain.common.usecase.UseCaseHandler;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ClearSeekerSuggestionsForPositionUseCaseHandler implements UseCaseHandler<Void, ClearSeekerSuggestionsForPosition> {
+
+    private SuggestionPort suggestionPort;
+
+    @Override
+    public Void handle(ClearSeekerSuggestionsForPosition useCase) {
+        return suggestionPort.clearAllSeekerSuggestions(useCase);
+    }
+}
--- src/main/java/com/tiklaisgelsin/api/domain/common/port/SuggestionPort.java
+++ src/main/java/com/tiklaisgelsin/api/domain/common/port/SuggestionPort.java
@@ -2,9 +2,12 @@ package com.tiklaisgelsin.api.domain.common.port;
 
 import com.tiklaisgelsin.api.domain.common.model.PositionSuggestion;
 import com.tiklaisgelsin.api.domain.common.model.SeekerSuggestion;
+import com.tiklaisgelsin.api.domain.common.usecase.ClearSeekerSuggestionsForPosition;
 
 public interface SuggestionPort {
     PositionSuggestion createPositionSuggestion(Long positionId, Long seekerId);
 
     SeekerSuggestion createSeekerSuggestion(Long seekerId, Long positionId);
+
+    Void clearAllSeekerSuggestions(ClearSeekerSuggestionsForPosition useCase);
 }
