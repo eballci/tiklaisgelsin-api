87c8c8a2762446e5c1119dd62ee3701fcbf72581
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-08 13:43:27 +0300

    Create position use case handlers for employer context

---
+++ src/main/java/com/tiklaisgelsin/api/domain/employer/handler/position/CreatePositionUseCaseHandler.java
@@ -0,0 +1,18 @@
+package com.tiklaisgelsin.api.domain.employer.handler.position;
+
+import com.tiklaisgelsin.api.domain.common.model.Position;
+import com.tiklaisgelsin.api.domain.common.usecase.UseCaseHandler;
+import com.tiklaisgelsin.api.domain.employer.port.PositionPort;
+import com.tiklaisgelsin.api.domain.employer.usecase.position.CreatePosition;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CreatePositionUseCaseHandler implements UseCaseHandler<Position, CreatePosition> {
+
+    private final PositionPort positionPort;
+
+    @Override
+    public Position handle(CreatePosition useCase) {
+        return positionPort.createPosition(useCase);
+    }
+}
---
+++ src/main/java/com/tiklaisgelsin/api/domain/employer/handler/position/DeletePositionUseCaseHandler.java
@@ -0,0 +1,17 @@
+package com.tiklaisgelsin.api.domain.employer.handler.position;
+
+import com.tiklaisgelsin.api.domain.common.usecase.UseCaseHandler;
+import com.tiklaisgelsin.api.domain.employer.port.PositionPort;
+import com.tiklaisgelsin.api.domain.employer.usecase.position.DeletePosition;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class DeletePositionUseCaseHandler implements UseCaseHandler<Void, DeletePosition> {
+
+    private final PositionPort positionPort;
+
+    @Override
+    public Void handle(DeletePosition useCase) {
+        return positionPort.removePosition(useCase);
+    }
+}
