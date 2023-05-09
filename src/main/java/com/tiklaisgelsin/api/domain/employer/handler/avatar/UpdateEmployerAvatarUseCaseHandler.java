7b239c6f35a8edf7e901da7d66ed87c4af9159af
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-09 22:52:42 +0300

    Cast Void return typed common employer avatar handlers to void ones

--- src/main/java/com/tiklaisgelsin/api/domain/employer/handler/avatar/UpdateEmployerAvatarUseCaseHandler.java
+++ src/main/java/com/tiklaisgelsin/api/domain/employer/handler/avatar/UpdateEmployerAvatarUseCaseHandler.java
@@ -1,6 +1,6 @@
 package com.tiklaisgelsin.api.domain.employer.handler.avatar;
 
-import com.tiklaisgelsin.api.domain.common.usecase.UseCaseHandler;
+import com.tiklaisgelsin.api.domain.common.usecase.VoidUseCaseHandler;
 import com.tiklaisgelsin.api.domain.employer.port.AvatarPort;
 import com.tiklaisgelsin.api.domain.employer.usecase.avatar.UpdateEmployerAvatar;
 import lombok.RequiredArgsConstructor;
@@ -8,12 +8,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class UpdateEmployerAvatarUseCaseHandler implements UseCaseHandler<Void, UpdateEmployerAvatar> {
+public class UpdateEmployerAvatarUseCaseHandler implements VoidUseCaseHandler<UpdateEmployerAvatar> {
 
     private final AvatarPort avatarPort;
 
     @Override
-    public Void handle(UpdateEmployerAvatar useCase) {
-        return avatarPort.updateSeekerAvatar(useCase);
+    public void handle(UpdateEmployerAvatar useCase) {
+        avatarPort.updateSeekerAvatar(useCase);
     }
 }
--- src/main/java/com/tiklaisgelsin/api/domain/employer/port/AvatarPort.java
+++ src/main/java/com/tiklaisgelsin/api/domain/employer/port/AvatarPort.java
@@ -3,5 +3,5 @@ package com.tiklaisgelsin.api.domain.employer.port;
 import com.tiklaisgelsin.api.domain.employer.usecase.avatar.UpdateEmployerAvatar;
 
 public interface AvatarPort {
-    Void updateSeekerAvatar(UpdateEmployerAvatar updateEmployerAvatar);
+    void updateSeekerAvatar(UpdateEmployerAvatar updateEmployerAvatar);
 }
