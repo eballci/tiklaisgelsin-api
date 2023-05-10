8127dcd4019c9fa0a25b63477964471ee0a22407
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-10 19:48:56 +0300

    Remove unnecessary use case handler and its affiliates

--- src/main/java/com/tiklaisgelsin/api/domain/employer/port/OfferPort.java
+++ src/main/java/com/tiklaisgelsin/api/domain/employer/port/OfferPort.java
@@ -1,11 +1,7 @@
 package com.tiklaisgelsin.api.domain.employer.port;
 
-import com.tiklaisgelsin.api.domain.common.model.Offer;
-import com.tiklaisgelsin.api.domain.employer.usecase.offer.IssueOffer;
 import com.tiklaisgelsin.api.domain.employer.usecase.offer.RemoveOffer;
 
 public interface OfferPort {
-    Offer issue(IssueOffer useCase);
-
     void remove(RemoveOffer useCase);
 }
