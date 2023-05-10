121948a9b119246b8fc81d7540ba95bcb973c8b9
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-10 19:44:46 +0300

    Remove unnecessary use case handler and its' affiliates

--- src/main/java/com/tiklaisgelsin/api/domain/seeker/port/SubmissionPort.java
+++ src/main/java/com/tiklaisgelsin/api/domain/seeker/port/SubmissionPort.java
@@ -1,11 +1,7 @@
 package com.tiklaisgelsin.api.domain.seeker.port;
 
-import com.tiklaisgelsin.api.domain.common.model.Submission;
-import com.tiklaisgelsin.api.domain.seeker.usecase.submission.IssueSubmission;
 import com.tiklaisgelsin.api.domain.seeker.usecase.submission.RemoveSubmission;
 
 public interface SubmissionPort {
-    Submission issueSubmission(IssueSubmission issueSubmission);
-
     void removeSubmission(RemoveSubmission removeSubmission);
 }
