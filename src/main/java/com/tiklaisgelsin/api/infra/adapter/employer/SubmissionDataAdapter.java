1edceed3007653f501a2c5b7236b871f5820a5d5
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-10 23:48:36 +0300

    Create submission adapter for employer ports

---
+++ src/main/java/com/tiklaisgelsin/api/infra/adapter/employer/SubmissionDataAdapter.java
@@ -0,0 +1,50 @@
+package com.tiklaisgelsin.api.infra.adapter.employer;
+
+import com.tiklaisgelsin.api.domain.common.model.SubmissionStatus;
+import com.tiklaisgelsin.api.domain.employer.port.SubmissionPort;
+import com.tiklaisgelsin.api.domain.employer.usecase.submission.AcceptSubmission;
+import com.tiklaisgelsin.api.domain.employer.usecase.submission.ReadAllSubmissions;
+import com.tiklaisgelsin.api.domain.employer.usecase.submission.RefuseSubmission;
+import com.tiklaisgelsin.api.infra.jpa.entity.SubmissionEntity;
+import com.tiklaisgelsin.api.infra.jpa.repository.SubmissionJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class SubmissionDataAdapter implements SubmissionPort {
+
+    private final SubmissionJpaRepository repository;
+
+    @Override
+    public void acceptSubmission(AcceptSubmission acceptSubmission) {
+        Optional<SubmissionEntity> submission = repository.findById(acceptSubmission.getSubmissionId());
+
+        if (submission.isEmpty()) return;
+
+        submission.get().setStatus(SubmissionStatus.ACCEPTED.getLevel());
+        repository.save(submission.get());
+    }
+
+    @Override
+    public void readAllSubmissions(ReadAllSubmissions readAllSubmissions) {
+        List<SubmissionEntity> submissionEntities = repository.findAllByEmployerId(readAllSubmissions.getEmployerId());
+        submissionEntities.forEach(submissionEntity -> {
+            submissionEntity.setStatus(SubmissionStatus.IDLE.getLevel());
+        });
+        repository.saveAll(submissionEntities);
+    }
+
+    @Override
+    public void refuseSubmission(RefuseSubmission refuseSubmission) {
+        Optional<SubmissionEntity> submission = repository.findById(refuseSubmission.getSubmissionId());
+
+        if (submission.isEmpty()) return;
+
+        submission.get().setStatus(SubmissionStatus.REFUSED.getLevel());
+        repository.save(submission.get());
+    }
+}
--- src/main/java/com/tiklaisgelsin/api/infra/jpa/repository/SubmissionJpaRepository.java
+++ src/main/java/com/tiklaisgelsin/api/infra/jpa/repository/SubmissionJpaRepository.java
@@ -3,5 +3,8 @@ package com.tiklaisgelsin.api.infra.jpa.repository;
 import com.tiklaisgelsin.api.infra.jpa.entity.SubmissionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubmissionJpaRepository extends JpaRepository<SubmissionEntity, Long> {
+    List<SubmissionEntity> findAllByEmployerId(Long employerId);
 }
