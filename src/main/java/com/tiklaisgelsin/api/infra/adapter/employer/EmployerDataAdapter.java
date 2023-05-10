1a944e75cb29a854c5666148ef211fb672f61119
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-10 23:28:01 +0300

    Create employer adapter for employer port

---
+++ src/main/java/com/tiklaisgelsin/api/infra/adapter/employer/EmployerDataAdapter.java
@@ -0,0 +1,52 @@
+package com.tiklaisgelsin.api.infra.adapter.employer;
+
+import com.tiklaisgelsin.api.domain.common.model.Employer;
+import com.tiklaisgelsin.api.domain.employer.port.EmployerPort;
+import com.tiklaisgelsin.api.domain.employer.usecase.employer.EmployerCreate;
+import com.tiklaisgelsin.api.domain.employer.usecase.employer.EmployerGet;
+import com.tiklaisgelsin.api.domain.employer.usecase.employer.EmployerUpdate;
+import com.tiklaisgelsin.api.infra.jpa.entity.EmployerEntity;
+import com.tiklaisgelsin.api.infra.jpa.repository.EmployerJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EmployerDataAdapter implements EmployerPort {
+
+    private final EmployerJpaRepository repository;
+
+    @Override
+    public Employer getEmployer(EmployerGet employerGet) {
+        return repository.findById(employerGet.getEmployerId())
+                .map(EmployerEntity::toModel)
+                .orElse(null);
+    }
+
+    @Override
+    public Employer createEmployer(EmployerCreate employerCreate) {
+        EmployerEntity employer = new EmployerEntity();
+        employer.setName(employerCreate.getName());
+        employer.setEmail(employerCreate.getEmail());
+        employer.setPassword(employerCreate.getPassword());
+
+        return repository.save(employer).toModel();
+    }
+
+    @Override
+    public void updateEmployer(EmployerUpdate employerUpdate) {
+        Optional<EmployerEntity> employer = repository.findById(employerUpdate.getEmployerId());
+
+        if (employer.isEmpty()) return;
+
+        employer.get().setName(employerUpdate.getName());
+        employer.get().setDescription(employerUpdate.getDescription());
+        employer.get().setEmail(employerUpdate.getEmail());
+        employer.get().setScale(employerUpdate.getScale());
+        employer.get().setWebSite(employerUpdate.getWebSite());
+
+        repository.save(employer.get());
+    }
+}
