79cd07e5bc27a420059a7017cd792a94285b3581
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-10 17:36:42 +0300

    Remove avatar infra side

--- src/main/java/com/tiklaisgelsin/api/infra/adapters/employer/avatar/jpa/repository/AvatarJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tiklaisgelsin.api.infra.adapters.employer.avatar.jpa.repository;
-
-import com.tiklaisgelsin.api.infra.adapters.employer.avatar.jpa.entity.AvatarEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AvatarJpaRepository extends JpaRepository<AvatarEntity, Long> {
-    AvatarEntity findByEmployerId(Long employerId);
-}
