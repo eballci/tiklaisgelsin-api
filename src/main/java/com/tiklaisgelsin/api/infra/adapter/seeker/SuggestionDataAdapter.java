71c11a40ccef19a25299f2a9f5d8aa6f0c483f70
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-11 12:27:10 +0300

    Create data adapter for seeker suggestion port

---
+++ src/main/java/com/tiklaisgelsin/api/infra/adapter/seeker/SuggestionDataAdapter.java
@@ -0,0 +1,43 @@
+package com.tiklaisgelsin.api.infra.adapter.seeker;
+
+import com.tiklaisgelsin.api.domain.common.model.Submission;
+import com.tiklaisgelsin.api.domain.common.model.SubmissionStatus;
+import com.tiklaisgelsin.api.domain.seeker.port.SuggestionPort;
+import com.tiklaisgelsin.api.domain.seeker.usecase.suggestion.IgnorePositionSuggestion;
+import com.tiklaisgelsin.api.domain.seeker.usecase.suggestion.SubmitSuggestion;
+import com.tiklaisgelsin.api.infra.jpa.entity.SubmissionEntity;
+import com.tiklaisgelsin.api.infra.jpa.entity.SuggestionEntity;
+import com.tiklaisgelsin.api.infra.jpa.repository.SubmissionJpaRepository;
+import com.tiklaisgelsin.api.infra.jpa.repository.SuggestionJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class SuggestionDataAdapter implements SuggestionPort {
+
+    private final SubmissionJpaRepository submissionJpaRepository;
+    private final SuggestionJpaRepository suggestionJpaRepository;
+
+    @Override
+    public Submission submitSuggestion(SubmitSuggestion submitSuggestion) {
+        Optional<SuggestionEntity> suggestion = suggestionJpaRepository
+                .findById(submitSuggestion.getSuggestionId());
+
+        if (suggestion.isEmpty()) return null;
+
+        SubmissionEntity submission = new SubmissionEntity();
+        submission.setStatus(SubmissionStatus.ISSUED.getLevel());
+        submission.setEmployer(suggestion.get().getEmployer());
+        submission.setPosition(suggestion.get().getPosition());
+        submission.setEmployer(suggestion.get().getEmployer());
+        return submissionJpaRepository.save(submission).toModel();
+    }
+
+    @Override
+    public void ignorePositionSuggestion(IgnorePositionSuggestion ignorePositionSuggestion) {
+        suggestionJpaRepository.deleteById(ignorePositionSuggestion.getSuggestionId());
+    }
+}
