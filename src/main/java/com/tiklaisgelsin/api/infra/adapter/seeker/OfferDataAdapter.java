e774d26a556b0ae5461a9a2e440bbd9c5c560bc9
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-11 11:35:10 +0300

    Create data adapter for seeker offer port

---
+++ src/main/java/com/tiklaisgelsin/api/infra/adapter/seeker/OfferDataAdapter.java
@@ -0,0 +1,52 @@
+package com.tiklaisgelsin.api.infra.adapter.seeker;
+
+import com.tiklaisgelsin.api.domain.common.model.OfferStatus;
+import com.tiklaisgelsin.api.domain.seeker.port.OfferPort;
+import com.tiklaisgelsin.api.domain.seeker.usecase.offer.AcceptOffer;
+import com.tiklaisgelsin.api.domain.seeker.usecase.offer.ReadAllOffers;
+import com.tiklaisgelsin.api.domain.seeker.usecase.offer.RefuseOffer;
+import com.tiklaisgelsin.api.infra.jpa.entity.OfferEntity;
+import com.tiklaisgelsin.api.infra.jpa.repository.OfferJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class OfferDataAdapter implements OfferPort {
+
+    private final OfferJpaRepository repository;
+
+    @Override
+    public void acceptOffer(AcceptOffer acceptOffer) {
+        Optional<OfferEntity> offer = repository.findById(acceptOffer.getOfferId());
+
+        if (offer.isEmpty()) return;
+
+        offer.get().setStatus(OfferStatus.ACCEPTED.getLevel());
+        repository.save(offer.get());
+    }
+
+    @Override
+    public void readAllOffers(ReadAllOffers readAllOffers) {
+        List<OfferEntity> offers = repository.findAllBySeekerId(readAllOffers.getSeekerId());
+
+        offers.forEach(offer -> {
+            offer.setStatus(OfferStatus.IDLE.getLevel());
+        });
+
+        repository.saveAll(offers);
+    }
+
+    @Override
+    public void refuseOffer(RefuseOffer refuseOffer) {
+        Optional<OfferEntity> offer = repository.findById(refuseOffer.getOfferId());
+
+        if (offer.isEmpty()) return;
+
+        offer.get().setStatus(OfferStatus.REFUSED.getLevel());
+        repository.save(offer.get());
+    }
+}
--- src/main/java/com/tiklaisgelsin/api/infra/jpa/repository/OfferJpaRepository.java
+++ src/main/java/com/tiklaisgelsin/api/infra/jpa/repository/OfferJpaRepository.java
@@ -3,5 +3,8 @@ package com.tiklaisgelsin.api.infra.jpa.repository;
 import com.tiklaisgelsin.api.infra.jpa.entity.OfferEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OfferJpaRepository extends JpaRepository<OfferEntity, Long> {
+    List<OfferEntity> findAllBySeekerId(Long seekerId);
 }
