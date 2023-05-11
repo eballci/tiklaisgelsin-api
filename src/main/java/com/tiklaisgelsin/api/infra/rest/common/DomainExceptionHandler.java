b19185bc2154f0993d7c8ea0ec2d181c8fe1acb2
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-11 19:57:26 +0300

    Make api more resilient when it comes to exceptions

---
+++ src/main/java/com/tiklaisgelsin/api/infra/rest/common/DomainExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.tiklaisgelsin.api.infra.rest.common;
+
+import com.tiklaisgelsin.api.domain.common.exception.DuplicateEntityException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class DomainExceptionHandler {
+
+    @ExceptionHandler(DuplicateEntityException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleDuplicateEntityException(
+            DuplicateEntityException ex,
+            HttpServletRequest request
+    ) {
+        return ErrorResponse.create(ex, HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+}
