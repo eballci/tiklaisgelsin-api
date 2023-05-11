63efb645aeb8339d9b1bfa6ced9c9acf87231b68
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-11 20:48:42 +0300

    Make missing entity exceptions handleable

--- src/main/java/com/tiklaisgelsin/api/infra/rest/common/DomainExceptionHandler.java
+++ src/main/java/com/tiklaisgelsin/api/infra/rest/common/DomainExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.tiklaisgelsin.api.infra.rest.common;
 
 import com.tiklaisgelsin.api.domain.common.exception.DuplicateEntityException;
+import com.tiklaisgelsin.api.domain.common.exception.MissingEntityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +15,12 @@ public class DomainExceptionHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleDuplicateEntityException(DuplicateEntityException ex) {
         return ErrorResponse.create(ex, HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+    @ExceptionHandler(MissingEntityException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handlerMissingEntityException(MissingEntityException ex) {
+        return ErrorResponse.create(ex, HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
 }
