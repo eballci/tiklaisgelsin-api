96bcb6a7637e8447af91832f44f95a113f20953b
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-13 23:55:17 +0300

    Complete offer repository phase

---
+++ src/main/java/com/tiklaisgelsin/api/infra/rest/common/controller/OfferController.java
@@ -0,0 +1,54 @@
+package com.tiklaisgelsin.api.infra.rest.common.controller;
+
+import com.tiklaisgelsin.api.domain.common.usecase.VoidUseCaseHandler;
+import com.tiklaisgelsin.api.domain.employer.usecase.offer.RemoveOffer;
+import com.tiklaisgelsin.api.domain.seeker.usecase.offer.AcceptOffer;
+import com.tiklaisgelsin.api.domain.seeker.usecase.offer.ReadAllOffers;
+import com.tiklaisgelsin.api.domain.seeker.usecase.offer.RefuseOffer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/offer/")
+public class OfferController {
+
+    private final VoidUseCaseHandler<RemoveOffer> removeOfferVoidUseCaseHandler;
+    private final VoidUseCaseHandler<AcceptOffer> acceptOfferVoidUseCaseHandler;
+    private final VoidUseCaseHandler<ReadAllOffers> readAllOffersVoidUseCaseHandler;
+    private final VoidUseCaseHandler<RefuseOffer> refuseOfferVoidUseCaseHandler;
+
+    @GetMapping("remove/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void removeOffer(@PathVariable Long id) {
+        removeOfferVoidUseCaseHandler.handle(RemoveOffer.builder()
+                .offerId(id)
+                .build());
+    }
+
+    @GetMapping("accept/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void acceptOffer(@PathVariable Long id) {
+        acceptOfferVoidUseCaseHandler.handle(AcceptOffer.builder()
+                .offerId(id)
+                .build());
+    }
+
+    @GetMapping("read/{seekerId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void readAllOffers(@PathVariable Long seekerId) {
+        readAllOffersVoidUseCaseHandler.handle(ReadAllOffers.builder()
+                .seekerId(seekerId)
+                .build());
+    }
+
+    @GetMapping("refuse/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void refuseOffer(@PathVariable Long id) {
+        refuseOfferVoidUseCaseHandler.handle(RefuseOffer.builder()
+                .offerId(id)
+                .build());
+    }
+
+}
